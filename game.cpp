f11b9532eaa1aa5c8023bb8d5347b86389f2bbb5
bearlimaknilu <93322195+bearlimaknilu@users.noreply.github.com>
2021-10-28 16:38:22 +0530

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -8,7 +8,7 @@ const int dy[4] = {-1, 0, 1, 0};
 const int N = 7;
 string grid[N][N];
 
-void read() {
+void read_grid() {
     for(int row = 0; row < N; ++row) {
         for(int col = 0; col < N; ++col) {
             cin >> grid[row][col];
@@ -20,6 +20,20 @@ bool inside(int row, int col) {
     return 0 <= row && row < N && 0 <= col && col < N;
 }
 
+struct Player {
+    int row, col;
+    void read() {
+        cin >> row >> col;
+    }
+};
+
 int main() {
-    read();
+    while(true) {
+        int turn_type;
+        cin >> turn_type;
+        read_grid();
+        Player me, he;
+        me.read();
+        he.read();
+    }
 }
